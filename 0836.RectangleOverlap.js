ecec6fc6858a25a9f79070b209b63d17b3294ceb
TroLOve-me <66876494+TroLOve-me@users.noreply.github.com>
2020-12-06 16:06:22 +0800

    Update 0836.RectangleOverlap.js
    have to consider if it is a rectangle or a line

--- 0836.RectangleOverlap.js
+++ 0836.RectangleOverlap.js
@@ -1,4 +1,5 @@
 var isRectangleOverlap = function (rec1, rec2) {
+if(rec1[0]===rec1[2] || rec1[1]===rec1[3] ||rec2[0]===rec2[2] || rec2[1]===rec2[3]) return false;
   if (
     rec1[2] <= rec2[0] ||
     rec1[1] >= rec2[3] ||
@@ -8,5 +9,5 @@ var isRectangleOverlap = function (rec1, rec2) {
     return false;
   } else {
     return true;
-  }
+  }  
 };
